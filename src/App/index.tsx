9b164db4adf3c8d09e528d439793f729bb6a9d90
Ross Bulat <ross@parity.io>
2024-06-09 10:48:07 +0700

    initial commit

---
+++ src/App/index.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import { AppWrapper } from '../Wrappers'
+import logo from 'svg/cloud.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <AppWrapper>
+      <div>
+        <a href="https://polkadot.cloud" target="_blank">
+          <img src={logo} className="logo" alt="Polkadot Cloud logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </AppWrapper>
+  )
+}
+
+export default App
---
+++ src/Wrappers.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+
+export const AppWrapper = styled.div`
+  max-width: 1280px;
+  margin: 0  auto;
+  padding: 2rem;
+  text-align: center;
+
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+
+    &:hover {
+      filter: drop-shadow(0 0 2em rgba(193, 14, 125, 0.15));
+    }
+  }
+
+  .card {
+    padding: 2em;
+  }
+
+  .read-the-docs {
+    color: #888;
+  }
+`;
