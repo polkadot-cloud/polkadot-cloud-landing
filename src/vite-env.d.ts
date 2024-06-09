f1199e82b2ddeb5daa204cd66e50aa513d293b52
Ross Bulat <ross@parity.io>
2024-06-09 10:57:11 +0700

    chore: update name

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,4 @@
-// Copyright 2024 @polkadot-cloud/landing authors & contributors
+// Copyright 2024 @polkadot-cloud/polkadot-cloud-landing authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
 /// <reference types="vite/client" />
